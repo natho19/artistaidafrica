acb74b2176285973ac0b3d099955c1afa0465a27
Nathan Akpawu <nakpawu@gmail.com>
2026-01-09 16:37:32 +0000

    add custom javascript file

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -5,10 +5,11 @@ function aaa_register_assets() {
     wp_register_style('aaa-style', get_template_directory_uri() . '/assets/css/style.css', ['aaa-main']);
 
     wp_register_script('aaa-libs', get_template_directory_uri() . '/assets/js/libs.min.js', [], false, true);
-    wp_register_script('aaa-common', get_template_directory_uri() . '/assets/js/common.min.js', ['aaa-libs', 'jquery'], false, true);
+    wp_register_script('aaa-common', get_template_directory_uri() . '/assets/js/common.min.js', [], false, true);
+    wp_register_script('aaa-script', get_template_directory_uri() . '/assets/js/script.js', ['jquery','aaa-libs','aaa-common'], false, true);
 
     wp_enqueue_style('aaa-style');
-    wp_enqueue_script('aaa-common');
+    wp_enqueue_script('aaa-script');
 }
 
 add_action('wp_enqueue_scripts', 'aaa_register_assets');
