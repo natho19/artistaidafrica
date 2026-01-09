f20bf69a07fd052c2102e33d0b2957f7d8c0622f
Nathan Akpawu <nakpawu@gmail.com>
2026-01-09 17:03:10 +0000

    add slides

--- index.php
+++ index.php
@@ -1,2 +1,125 @@
 <?php get_header() ?>
+    <!-- promo start-->
+    <section class="promo">
+        <div class="promo-slider">
+            <div class="promo-slider__item promo-slider__item--style-1">
+                <picture>
+                    <source srcset="<?= AAA_IMG_DIR . 'promo_1.jpg' ?>" media="(min-width: 835px)" />
+                    <source srcset="<?= AAA_IMG_DIR . '834promo_1.jpg' ?>" media="(min-width: 376px)" />
+                    <img class="img--bg" src="<?= AAA_IMG_DIR . '375promo_1.jpg' ?>" alt="img" />
+                </picture>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="align-container">
+                                <div class="align-container__item">
+                                    <div class="promo-slider__wrapper-1">
+                                        <h2 class="promo-slider__title">
+                                            <span>We help all people in need</span>
+                                            <span>around the world.</span>
+                                        </h2>
+                                    </div>
+                                    <div class="promo-slider__wrapper-2">
+                                        <p class="promo-slider__subtitle">Gray eel-catfish longnose whiptail catfish smalleye squaretail queen danio unicorn fish shortnose greeneye fusilier fish silver carp nibbler sharksucker tench lookdown catfish</p>
+                                    </div>
+                                    <div class="promo-slider__wrapper-3">
+                                        <a class="button promo-slider__button button--primary" href="#">Discover</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="promo-slider__item promo-slider__item--style-2">
+                <picture>
+                    <source srcset="<?= AAA_IMG_DIR . 'promo_2.jpg' ?>" media="(min-width: 835px)" />
+                    <source srcset="<?= AAA_IMG_DIR . '834promo_2.jpg' ?>" media="(min-width: 376px)" />
+                    <img class="img--bg" src="<?= AAA_IMG_DIR . '375promo_2.jpg' ?>" alt="img" />
+                </picture>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-7">
+                            <div class="align-container">
+                                <div class="align-container__item">
+                                    <div class="promo-slider__wrapper-1">
+                                        <h2 class="promo-slider__title">
+                                        <span>Our Helping</span><br />
+                                        <span>around the world.</span></h2>
+                                    </div>
+                                    <div class="promo-slider__wrapper-2">
+                                        <p class="promo-slider__subtitle">Gray eel-catfish longnose whiptail catfish smalleye squaretail queen danio unicorn fish shortnose greeneye fusilier fish silver carp nibbler sharksucker tench lookdown catfish</p>
+                                    </div>
+                                    <div class="promo-slider__wrapper-3">
+                                        <a class="button promo-slider__button button--primary" href="#">Discover</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- promo socials start-->
+        <ul class="promo-socials">
+            <li class="promo-socials__item">
+                <a class="promo-socials__link" href="#">
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+            </li>
+            <li class="promo-socials__item">
+                <a class="promo-socials__link" href="#">
+                    <i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                </a>
+            </li>
+            <li class="promo-socials__item">
+                <a class="promo-socials__link" href="#">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                </a>
+            </li>
+            <li class="promo-socials__item">
+                <a class="promo-socials__link" href="#">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+            </li>
+        </ul>
+        <!-- promo socials end-->
+        <!-- promo pannel start-->
+        <div class="promo-pannel">
+            <a class="anchor promo-pannel__anchor" href="#about">
+                <span>Scroll Down</span>
+            </a>
+            <div class="promo-pannel__video">
+                <img class="img--bg" src="<?= AAA_IMG_DIR . 'video_block.jpg' ?>" alt="image" />
+                <a class="video-trigger" href="https://www.youtube.com/watch?v=_sI_Ps7JSEk">
+                    <span>Watch our video</span>
+                    <i class="fa fa-play" aria-hidden="true"></i>
+                </a>
+            </div>
+            <div class="promo-pannel__phones">
+                <p class="promo-pannel__title">Phone numbers</p>
+                <a class="promo-pannel__link" href="tel:+180012345678">+ 1800 - 123 456 78</a>
+                <a class="promo-pannel__link" href="tel:+18009756511">+ 1800 - 975 65 11</a>
+            </div>
+            <div class="promo-pannel__email">
+                <p class="promo-pannel__title">Email</p>
+                <a class="promo-pannel__link" href="mailto:support@helpo.org">support@helpo.org</a>
+            </div>
+        </div>
+        <!-- promo pannel end-->
+        <!-- slider nav start-->
+        <div class="slider__nav slider__nav--promo">
+            <div class="promo-slider__count"></div>
+            <div class="slider__arrows">
+                <div class="slider__prev">
+                    <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                </div>
+                <div class="slider__next">
+                    <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+        <!-- slider nav end-->
+    </section>
+    <!-- promo end-->
 <?php get_footer() ?>
