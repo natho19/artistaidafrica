50ce10767b7efdd3d5b94f1ec6b6725ceee35fe8
Nathan Akpawu <nakpawu@gmail.com>
2026-05-07 15:35:48 +0000

    dynamic testimonials section on homepage

--- templates/homepage.php
+++ templates/homepage.php
@@ -8,8 +8,8 @@
         <div class="promo-slider">
             <?php while (have_rows('slides')) : the_row(); ?>
                 <div class="promo-slider__item promo-slider__item--style-1">
-                    <?php 
-                    $background = get_sub_field('background'); 
+                    <?php
+                    $background = get_sub_field('background');
                     if (!empty($background)) : ?>
                         <picture>
                             <img class="img--bg" src="<?= esc_url($background['url']); ?>" alt="<?= esc_attr($background['alt']); ?>" />
@@ -29,9 +29,9 @@
                                             </div>
                                         <?php endif; ?>
                                         <?php if (get_sub_field('description')) : ?>
-                                        <div class="promo-slider__wrapper-2">
-                                            <p class="promo-slider__subtitle"><?= esc_html(get_sub_field('description')); ?></p>
-                                        </div>
+                                            <div class="promo-slider__wrapper-2">
+                                                <p class="promo-slider__subtitle"><?= esc_html(get_sub_field('description')); ?></p>
+                                            </div>
                                         <?php endif; ?>
                                         <?php if (get_sub_field('link')) : ?>
                                             <div class="promo-slider__wrapper-3">
@@ -100,9 +100,9 @@
 
 <!-- about us start-->
 <section id="about" class="section about-us">
-	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-lg-6">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
                 <?php if (get_field('home_about_title')) : ?>
                     <div class="heading heading--primary">
                         <h2 class="heading__title"><span><?= esc_html(get_field('home_about_title')); ?></span></h2>
@@ -116,10 +116,10 @@
                 if ($about_link) : ?>
                     <a class="button button--primary" href="<?= esc_url($about_link['url']) ?>"><?= esc_html($about_link['title']) ?></a>
                 <?php endif; ?>
-			</div>
+            </div>
 
-            <?php 
-            $about_image = get_field('home_about_image'); 
+            <?php
+            $about_image = get_field('home_about_image');
             if (!empty($about_image)) : ?>
                 <div class="col-lg-6 col-xl-5 offset-xl-1">
                     <div class="info-box">
@@ -127,14 +127,14 @@
                     </div>
                 </div>
             <?php endif; ?>
-		</div>
-	</div>
+        </div>
+    </div>
 </section>
 <!-- about us end-->
 
 <!-- section start-->
 <section class="section background--brown">
-	<div class="container">
+    <div class="container">
         <?php if (get_field('what_we_do_title')) : ?>
             <div class="row margin-bottom">
                 <div class="col-12">
@@ -145,17 +145,17 @@
             </div>
         <?php endif; ?>
 
-        <?php if( have_rows('what_we_do') ): ?>
+        <?php if (have_rows('what_we_do')): ?>
             <div class="row">
-                <?php while( have_rows('what_we_do') ): the_row(); ?>
+                <?php while (have_rows('what_we_do')): the_row(); ?>
                     <div class="col-6 col-md-4 col-lg-3">
                         <div class="icon-item">
-                            <?php 
+                            <?php
                             $illustration = get_sub_field('illustration');
                             if ($illustration) : ?>
-                            <div class="icon-item__img">
-                                <img src="<?= esc_url($illustration['url']); ?>" alt="<?= esc_attr($illustration['alt']); ?>" />
-                            </div>
+                                <div class="icon-item__img">
+                                    <img src="<?= esc_url($illustration['url']); ?>" alt="<?= esc_attr($illustration['alt']); ?>" />
+                                </div>
                             <?php endif; ?>
                             <?php if (get_sub_field('description')) : ?>
                                 <div class="icon-item__text">
@@ -177,83 +177,87 @@
                 </div>
             </div>
         <?php endif; ?>
-	</div>
+    </div>
 </section>
 <!-- section end-->
 
-<!-- section start-->
 <section class="section action-section">
-	<div class="jarallax">
-		<picture>
-            <img class="jarallax-img" src="<?= AAA_IMG_DIR . 'hands.png' ?>" alt="img" />
-		</picture>
-	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-12 text-center">
-				<h2 class="action-section__title">Any information?</h2>
-				<p class="action-section__text">Have questions or want to learn more about ArtistAid Africa? We're here to help. Reach out to us for partnership opportunities, donations, or general inquiries, and let's work together to empower artists and communities through art.</p>
-                <a class="action-section__button button button--primary" href="http://localhost/artistaidafrica/contact/">Contact Us</a>
-			</div>
-		</div>
-	</div>
+    <?php $any_information_background = get_field('any_information_background');
+    if ($any_information_background): ?>
+        <div class="jarallax">
+            <img class="jarallax-img" src="<?= esc_url($any_information_background['url']); ?>" alt="<?= esc_attr($any_information_background['alt']); ?>" />
+        </div>
+    <?php endif; ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <?php if (get_field('any_information_title')): ?>
+                    <h2 class="action-section__title">
+                        <?= esc_html(get_field('any_information_title')); ?>
+                    </h2>
+                <?php endif; ?>
+
+                <?php if (get_field('any_information_description')): ?>
+                    <p class="action-section__text">
+                        <?= esc_html(get_field('any_information_description')); ?>
+                    </p>
+                <?php endif; ?>
+
+                <?php $any_information_link = get_field('any_information_link');
+                if ($any_information_link): ?>
+                    <a class="action-section__button button button--primary" href="<?= esc_url($any_information_link['url']); ?>">
+                        <?= esc_html($any_information_link['title']); ?>
+                    </a>
+                <?php endif; ?>
+
+            </div>
+        </div>
+    </div>
 </section>
-<!-- section end-->
 
 <!-- testimonials style-1 start-->
-<section class="section testimonials">
-	<div class="container">
-		<div class="row align-items-end margin-bottom">
-			<div class="col-lg-8 col-xl-7 offset-xl-1">
-				<div class="heading heading--primary">
-					<h2 class="heading__title"><span>Testimonials</span></h2>
-				</div>
-			</div>
-			<div class="col-lg-4 col-xl-3">
-				<div class="slider__nav testimonials-style-1__nav">
-					<div class="slider__arrows">
-						<div class="slider__prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>
-						<div class="slider__next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xl-10 offset-xl-1">
-				<div class="testimonials-slider testimonials-slider--style-1">
-					<div class="testimonials-slider__item">
-						<div class="testimonials-slider__icon">“</div>
-						<div class="testimonials-slider__text">
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dicta eos alias. Nesciunt tempora esse fugiat perspiciatis illo ducimus eius commodi, eos inventore, doloremque cupiditate dolor maxime fugit voluptas dolorum odit dolores illum repellendus sequi nihil at eligendi veritatis. Suscipit.</p>
-							<div class="testimonials-slider__author">
-                                <span class="testimonials-slider__name">Jack Wolfskin</span>
+<?php $testimonials = get_field('testimonials'); if ($testimonials): ?>
+    <section class="section testimonials">
+        <div class="container">
+            <div class="row align-items-end margin-bottom">
+                <?php if (get_field('testimonials_title')): ?>
+                    <div class="col-lg-8 col-xl-7 offset-xl-1">
+                        <div class="heading heading--primary">
+                            <h2 class="heading__title"><span><?= esc_html(get_field('testimonials_title')); ?></span></h2>
+                        </div>
+                    </div>
+                <?php endif; ?>
+                <div class="col-lg-4 col-xl-3">
+                    <div class="slider__nav testimonials-style-1__nav">
+                        <div class="slider__arrows">
+                            <div class="slider__prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>
+                            <div class="slider__next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-10 offset-xl-1">
+                    <div class="testimonials-slider testimonials-slider--style-1">
+                        <?php foreach ($testimonials as $item): ?>
+                            <div class="testimonials-slider__item">
+                                <div class="testimonials-slider__icon">“</div>
+                                <div class="testimonials-slider__text">
+                                    <p><?= esc_html($item['testimonial']); ?></p>
+                                    <div class="testimonials-slider__author">
+                                        <span class="testimonials-slider__name"><?= esc_html($item['author']); ?></span>
+                                    </div>
+                                </div>
                             </div>
-						</div>
-					</div>
-					<div class="testimonials-slider__item">
-						<div class="testimonials-slider__icon">“</div>
-						<div class="testimonials-slider__text">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, repellat, adipisci quaerat provident iure beatae velit mollitia maxime dolorem soluta totam placeat iste ipsa sequi voluptatem quos dolor, cumque accusantium nobis nulla temporibus assumenda ad asperiores! Error eligendi voluptas consequatur.</p>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum laborum perferendis enim consequuntur! Ea iure rerum facilis, maiores adipisci minus!</p>
-							<div class="testimonials-slider__author">
-                                <span class="testimonials-slider__name">Jack Wolfskin</span>
-                            </div>
-						</div>
-					</div>
-					<div class="testimonials-slider__item">
-						<div class="testimonials-slider__icon">“</div>
-						<div class="testimonials-slider__text">
-							<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aliquid ut at eveniet beatae molestias placeat ullam id magni sit?</p>
-							<div class="testimonials-slider__author">
-                                <span class="testimonials-slider__name">Jack Wolfskin</span>
-                            </div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
 <!-- testimonials style-1 end-->
+
 
 <?php get_footer() ?>
